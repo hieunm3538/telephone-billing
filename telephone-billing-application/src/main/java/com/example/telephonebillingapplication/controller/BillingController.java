f645b81bbeefc7ccc72088528da7e9f2ac573367
Hiệu Nguyễn <hieu.nguyen20@onemount.com>
2022-12-08 09:47:51 +0700

    Update BaseResponse

--- telephone-billing-application/src/main/java/com/example/telephonebillingapplication/controller/BillingController.java
+++ telephone-billing-application/src/main/java/com/example/telephonebillingapplication/controller/BillingController.java
@@ -1,6 +1,7 @@
 package com.example.telephonebillingapplication.controller;
 
 import com.example.telephonebillingapplication.business.IBillingService;
+import com.example.telephonebillingapplication.dto.BaseResponse;
 import com.example.telephonebillingapplication.dto.request.CallRecordRequest;
 import com.example.telephonebillingapplication.dto.response.BillingResponse;
 import com.example.telephonebillingapplication.dto.response.CallRecordResponse;
@@ -16,18 +17,18 @@ public class BillingController {
     private final IBillingService service;
 
     @PostMapping("/{username}/call")
-    public CallRecordResponse recordCallDuration(@PathVariable(name = "username") @NonNull String username,
-                                                 @RequestBody CallRecordRequest request) {
+    public BaseResponse<CallRecordResponse> recordCallDuration(@PathVariable(name = "username") @NonNull String username,
+                                                              @RequestBody CallRecordRequest request) {
         if (username.length() > 32)
             throw new IllegalArgumentException("Username's length must be under 32 characters!");
-        return service.recordCallDuration(username, request);
+        return BaseResponse.ofSucceeded(service.recordCallDuration(username, request));
     }
 
     @GetMapping("/{username}/billing")
-    public BillingResponse getBilling(@PathVariable(name = "username") @NonNull String username) {
+    public BaseResponse<BillingResponse> getBilling(@PathVariable(name = "username") @NonNull String username) {
         if (username.length() > 32)
             throw new IllegalArgumentException("Username's length must be under 32 characters!");
-        return service.getBilling(username);
+        return BaseResponse.ofSucceeded(service.getBilling(username));
     }
 
 }
